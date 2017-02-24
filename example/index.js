8195bc2c35a45d1078049226c606edfa93da8617
Zachary Smith <william.smith1@wipro.com>
2017-02-24 16:52:28 -0500

    Clean up example

--- example/index.js
+++ example/index.js
@@ -2,13 +2,13 @@ const token2patternlab = require("../lib");
 
 console.log(token2patternlab(
   [ { name: "Button" }
-  , { description: "This is a description" }
+  , { description: "This is a description. And here is a code snippet:" }
   , { "code": {
         language: ""
       , content: [
           "<Button"
-        , "onClick=handler"
-        , "\>"
+        , "  onClick={handler}"
+        , "/\>"
         ]
       }
     }
